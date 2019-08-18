1725d680db378f2be0522aebb623729b9216e833
spikeburton <ralphburton3@gmail.com>
2019-08-18 15:30:04 -0400

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+25.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  User.create(f_name: first_name, l_name: last_name)
+end
